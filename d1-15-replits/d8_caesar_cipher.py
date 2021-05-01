eb021ecdac34990af1ba957b47ffc3d98bbdca6b
hariprasadmenon <menon.hariprasad@gmail.com>
2021-05-01 18:48:36 -0400

    improve ux & more refactoring

--- d1-15-replits/d8_caesar_cipher.py
+++ d1-15-replits/d8_caesar_cipher.py
@@ -1,27 +1,39 @@
+import replit
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+other = [' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caeser(plain_text, shift_amount, user_direction):
+    if shift_amount >= 26:
+      shift_amount = shift_amount%26
+    final_msg = []
+    if user_direction =="decode":
+      shift_amount*= -1
+    for i in text:
+        if i in other:
+            final_msg.append(i)
+        elif alphabet.index(i) + shift_amount < 0:
+            letter = alphabet.index(i)
+            final_msg.append(alphabet[letter+shift_amount+26])
+        elif alphabet.index(i) + shift_amount > 25:
+            letter = alphabet.index(i)
+            final_msg.append(alphabet[letter+shift_amount-26])
+        else:
+            letter = alphabet.index(i)
+            final_msg.append(alphabet[letter+shift_amount])
 
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-def ceaser(plain_text, shift_amount, user_direction):
-  final_msg = []
-  shift_amount*= -1 if user_direction =="decode" else shift_amount
-  for i in text:
-      if i == " ":
-          final_msg.append(" ")
-      elif alphabet.index(i) + shift_amount < 0:
-          letter = alphabet.index(i)
-          final_msg.append(alphabet[letter+shift_amount+26])
-      elif alphabet.index(i) + shift_amount > 25:
-          letter = alphabet.index(i)
-          final_msg.append(alphabet[letter+shift_amount-26])
-      else:
-          letter = alphabet.index(i)
-          final_msg.append(alphabet[letter+shift_amount])
-  if user_direction == "encode":
-    print(f"The encoded text is {''.join(final_msg)}")
-  else:
-    print(f"The decoded text is {''.join(final_msg)}")
-ceaser(plain_text = text, shift_amount = shift, user_direction = direction)
+    print(f"The {user_direction}d text is: {''.join(final_msg)}")
+
+go_again = "yes"
+while go_again == "yes":
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caeser(plain_text=text, shift_amount=shift, user_direction=direction)
+    go_again = input("Do you want to continue, 'yes' or 'no'?: ").lower()
+    if go_again == "yes":
+      replit.clear()
+    else:
+      print("Goodbye!")
+
